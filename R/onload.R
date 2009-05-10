4f7cc25ace83330c9017b8a731a163f715a0d6ed
David Ardia <david.ardia@unifr.ch>
2009-05-10 00:00:00 +0000

    version 1-01.02

--- R/onload.R
+++ R/onload.R
@@ -1,7 +1,7 @@
 '.onLoad' <- function(lib, pkg="AdMit")
   {
     cat("\nLoading 'AdMit' package...\n")
-    cat("Version    : 1.00-05\n")
+    cat("Version    : 1.01-02\n")
     cat("Authors    : David Ardia, Lennart F. Hoogerheide and Herman K. van Dijk\n")
     cat("Maintainer : David Ardia <david.ardia@unifr.ch>\n")
   }
